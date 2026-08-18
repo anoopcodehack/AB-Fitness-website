11c55014ef5fc68ec18277392fe31ceaf3d764de
Anoop-A <anooparjun05@gmail.com>
2026-08-18 19:06:02 +0530

    "feat: fixed error"

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: './',
+    base: mode === 'production' ? '/' : './',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
